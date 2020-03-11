d494e3fce67a5c62e41b1997ac6f7fef389d2314
rkrahul523 <rkrahul523@gmail.com>
2020-03-11 20:00:19 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { TableComponent } from './components/table/table.component';
-import { FiltercomponentComponent } from './components/filtercomponent/filtercomponent.component';
-import { OtpexampleComponent } from './components/otpexample/otpexample.component';
-
 @NgModule({
   imports:      [ 
     BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TableComponent, FiltercomponentComponent, OtpexampleComponent ],
+  declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
